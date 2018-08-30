2a36e60eebeaa181c8ebdb91b672305c7c6ce2d9
Louis PIRES--AGIER <louis@pockost.com>
2018-08-30 10:54:06 +0200

    :art: replace static mount points by env var

--- 1.6.0/entrypoint.sh
+++ 1.6.0/entrypoint.sh
@@ -1,7 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 export INFLUX_TELEGRAF_HOST=$(curl -s --unix-socket /var/run/docker.sock http/info | jq -r '.Name')
+
+if [[ -n "$MOUNT_POINTS" ]]; then
+
+  IFS=', ' read -r -a mount_points <<< $MOUNT_POINTS
+
+  result=""
+
+  for i in "${mount_points[@]}"; do
+    if [[ -z "$result" ]]; then
+      result="\"$i\""
+    else
+      result="$result, \"$i\""
+    fi
+  done
+
+  sed -i "s/\#\ mount_points\ =\ \[\"\/\"\]/mount_points\ =\ \[$result\]/" /etc/telegraf/telegraf.conf
+
+fi
 
 if [ "${1:0:1}" = '-' ]; then
   set -- telegraf "$@"
